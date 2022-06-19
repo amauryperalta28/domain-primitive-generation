4f0fc790e643754c4cebff1784a3f448dfb5b1f8
amauryperalta28 <amauryperalta28@gmail.com>
2022-06-19 09:54:01 -0400

    Initial commit

---
+++ tests/App.test.ts
@@ -0,0 +1,10 @@
+
+// export { };
+
+describe('App tests', () => {
+    test('Testing jest', () => {
+        const actual = (5 / 5);
+        const expected = 1;
+        expect(actual).toBe(expected);
+    })
+});
