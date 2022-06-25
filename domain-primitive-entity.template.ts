55f2d16a94d9324ddf9e2dd2b0c9c69a6edaa6c9
amauryperalta28 <amauryperalta28@gmail.com>
2022-06-25 00:18:02 -0400

    Finish domain primitive entity template

--- domain-primitive-entity.template.ts
+++ domain-primitive-entity.template.ts
@@ -102,14 +102,69 @@ const writeDomainPrimitiveEntity = (
     customWriter.writePrivateConstructor(className, parameters, null);
     customWriter.writeCodeBlock(()=>{
       customWriter.writeLine('Arguments.NotNull(builder, nameof(builder));');
+
+      properties.forEach((property)=>{
+            customWriter.writeLine(`${property.name} = builder.${property.name}Option.ValueOrFailure();`);
+      })
     });
     customWriter.writeLine();
+
+    writeEntityBuilder(className, customWriter, properties);
 
 
     //TODO: Agregar inicializacion de campos desde el builder
   });
 
-  //Escribir autproperties publicas solo con get
 };
+
+const writeEntityBuilder = (className: string, customWriter: CustomCsharpWriter,
+  properties: DomainPrimitiveProperty[])=>{
+  const classDefinitions: ClassDefinition = {
+    name: 'Builder',
+    accessModifier: 'public',
+    inherits:['AbstractEntityBuilder<Domain>'],
+    xmlDocSummary: [`${className}'s builder.`],
+  };
+  customWriter.writeClassBlock(classDefinitions, (c) => {
+    
+    customWriter.writeLine('protected override Option<string> AlreadyBuiltErrorMessage => Option.None<string>();');
+    customWriter.writeLine('protected override Option<string> MustBeBuiltErrorMessage => Option.None<string>();');
+
+    properties.forEach((property)=>{
+      customWriter.writeLine(`internal Option<${property.name}> ${property.name}Option { get; private set; }`);
+      customWriter.writeLine();
+
+    });
+
+    customWriter.writeLine('private new Builder SetProperty(Action setter) => (Builder)base.SetProperty(setter);');
+    customWriter.writeLine();
+
+    writeDoBuild(className, customWriter, properties);
+
+    properties.forEach((property)=>{
+      writeWithMethod(property.name, customWriter);
+    });
+
+    //TODO: Agregar inicializacion de campos desde el builder
+  });
+}
+
+const writeWithMethod = (className: string, customWriter: CustomCsharpWriter)=>{
+   const propertyName = className.toLowerCase();
+   customWriter.writeLine(`public Builder With${className}(${className} ${propertyName})`);
+   customWriter.writeLine(`    => SetProperty(() => ${className}Option = Arguments.NotNull(${propertyName}, nameof(${propertyName}).SomeNotNull()));`);
+}
+
+const writeDoBuild = (className: string, customWriter: CustomCsharpWriter, properties: DomainPrimitiveProperty[])=>{
+   customWriter.writeLine(`protected override ${className} DoBuild()`);
+   customWriter.writeCodeBlock(()=>{
+    properties.forEach((property)=>{
+      customWriter.writeLine(`State.IsTrue(${property.name.toLowerCase()}Option.HasValue, "${className}'s ${property.name} is missing");`)
+      
+    })
+   });
+
+   customWriter.writeLine();
+}
 
 //TODO: ver como usar nodemon para compilar cambios inmediatamente
