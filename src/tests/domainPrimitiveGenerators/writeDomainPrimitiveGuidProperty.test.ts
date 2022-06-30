3d8c454d53cf48744c3c891cce864419b59dabc5
amauryperalta28 <amauryperalta28@gmail.com>
2022-06-30 17:13:02 -0400

    Refactored writeDomainPrimitiveGuidProperty.test

--- src/tests/domainPrimitiveGenerators/writeDomainPrimitiveGuidProperty.test.ts
+++ src/tests/domainPrimitiveGenerators/writeDomainPrimitiveGuidProperty.test.ts
@@ -2,20 +2,27 @@ import * as fs from 'fs';
 
 describe('writeDomainPrimitiveGuidProperty.ts tests', () => {
   test('When a required guid domain primitive property is generated should generate property template correctly', (done) => {
-    fs.readFile(__dirname + '/template-test-result/User/Id.cs', 'utf8', function (_errFirst, generatedFile) {
-    
-      fs.readFile(
-        __dirname + '/template/IdGuidRequiredProperty.cs',
-        'utf8',
-        function (_err, template) {
-          done();
-          expect(generatedFile).toBeDefined();
-          expect(template).toBeDefined();
-          expect(generatedFile).toEqual(template);
-          
-        }
-      );
-    });
+   const generatedTemplateFilePath  = __dirname + '/template-test-result/User/Id.cs';
+   const expectedTemplateFilePath =  __dirname + '/template/IdGuidRequiredProperty.cs'
+
+    compareGeneratedTemplateWithExpectedTemplate(generatedTemplateFilePath, expectedTemplateFilePath, done);
   });
 
 });
+
+const compareGeneratedTemplateWithExpectedTemplate = (generatedFilePath: string, templatePath: string, done: Function)=>{
+  fs.readFile(generatedFilePath, 'utf8', function (_errFirst, generatedFile) {
+    
+    fs.readFile(
+      templatePath,
+      'utf8',
+      function (_err, template) {
+        done();
+        expect(generatedFile).toBeDefined();
+        expect(template).toBeDefined();
+        expect(generatedFile).toEqual(template);
+        
+      }
+    );
+  });
+}
